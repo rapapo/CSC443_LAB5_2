771b516fd90aa5b55d8d4a6f8ae00d6942eaff5d
rapapo <asyraf.akif91k@gmail.com>
2022-05-20 18:14:39 +0800

    second commit

--- assocArray.php
+++ assocArray.php
@@ -2,25 +2,15 @@
 <html>
 <body>
 <?php
-$month = array ("January","February","March","April","May","June","July","August","September","October","November","December");
-$monthDays = array ('January' => 31, 'February' => 28, 'March' => 31, 'April' => 30,'May' => 31, 'June' => 30, 'July' => 31, 'August' => 31,'September' => 30, 'October' => 31, 'November'=> 30, 'December' => 31)
-;$i = 0;
-foreach( $monthDays as $item )
-{
-    echo "$month[$i] - $item <br>";
-    $i++;
-}echo "<br>";
 $month = array ('January', 'February', 'March', 'April','May', 'June', 'July', 'August','September', 'October', 'November', 'December');
 $monthDays = array ('January' => 31, 'February' => 28, 'March' => 31, 'April' => 30,'May' => 31, 'June' => 30, 'July' => 31, 'August' => 31,'September' => 30, 'October' => 31, 'November'=> 30, 'December' => 31)
 ;$i = 0;
-foreach( $month as $item )
+foreach($monthDays as $item => $days)
 {
-    if($item == 30)
-    {
-         echo "$month[$i] - $item <br>";
-}
-     $i++;
-}
+        echo "$month[$i] - $days <br>";
+        $i++;
+}echo "<br>";
+
 ?>
 </body>
 </html>
