65d0992acdd89bbd940f381c80298d8090c85da7
rapapo <asyraf.akif91k@gmail.com>
2022-05-20 17:37:10 +0800

    Sorting

--- indexedArray.php
+++ indexedArray.php
@@ -5,6 +5,7 @@
 $month = array ("January","February","March","April","May","June","July","August","September","October","November","December");
 $len = count($month);
 for($i=0;$i<$len;$i++){
+    sort($month); // sort month alphabetical
     echo $month[$i];
     echo"<br>";
 }
