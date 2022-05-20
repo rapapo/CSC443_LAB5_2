ec0cacf587f099f2e8a2696a6572982d8dfbc52f
rapapo <asyraf.akif91k@gmail.com>
2022-05-20 17:30:02 +0800

    second commit

--- indexedArray.php
+++ indexedArray.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 <?php
-$month = array ("January",""February","March","April","May","June","July","August","September","October","November","December");
+$month = array ("January","February","March","April","May","June","July","August","September","October","November","December");
 $len = count($month);
 for($i=0;$i<$len;$i++){
     echo $month[$i];
