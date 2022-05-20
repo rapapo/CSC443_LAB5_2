66818ce8476bd2e8877eee7d4921c8f9a3d04893
rapapo <asyraf.akif91k@gmail.com>
2022-05-20 17:29:13 +0800

    first commit

---
+++ indexedArray.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$month = array ("January",""February","March","April","May","June","July","August","September","October","November","December");
+$len = count($month);
+for($i=0;$i<$len;$i++){
+    echo $month[$i];
+    echo"<br>";
+}
+?>
+</body>
+</html>
